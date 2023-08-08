80f8a6ad2722b6c62f181e42ce27fc920c8f0a7d
Theofilos Spyrou <alfatheo21@gmail.com>
2023-08-08 20:37:04 +0200

    Create quantization utilities

---
+++ spikefi/utils/quantization.py
@@ -0,0 +1,33 @@
+class Quantizer:
+    def __init__(self, precision: int, xmin: float, xmax: float, qmin: int = 0) -> None:
+        self.precision = precision
+        self.xmin = xmin
+        self.xmax = xmax
+
+        self.qmin = qmin
+        self.qmax = 2. ** self.precision - 1 - self.qmin
+
+        self.scale = (self.xmax - self.xmin) / (self.qmax - self.qmin)
+
+        self.zero_point = self.qmin - self.xmin / self.scale
+        if self.zero_point < self.qmin:
+            self.zero_point = self.qmin
+        elif self.zero_point > self.qmax:
+            self.zero_point = self.qmax
+        self.zero_point = int(self.zero_point)
+
+    def quantize(self, x: float) -> int:
+        assert x >= self.xmin and x <= self.xmax
+
+        q = int(self.zero_point + x / self.scale)
+
+        if q < self.qmin:
+            return self.qmin
+        if q > self.qmax:
+            return self.qmax
+        return q
+
+    def dequantize(self, q: int) -> float:
+        assert q >= self.qmin and q <= self.qmax
+
+        return self.scale * (q - self.zero_point)
