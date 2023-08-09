955d257591acb4bcec38d20c10b218e60a2a27ed
Theofilos Spyrou <alfatheo21@gmail.com>
2023-08-09 16:06:44 +0200

    Speed up inference and unify layer names

--- example/nmnist_cnn/nmnist.py
+++ example/nmnist_cnn/nmnist.py
@@ -44,22 +44,22 @@ class nmnistNetwork(torch.nn.Module):
         slayer = snn.layer(netParams['neuron'], netParams['simulation'])
         self.slayer = slayer
         # define network functions
-        self.conv1 = slayer.conv(2, 16, 5, padding=1)
-        self.conv2 = slayer.conv(16, 32, 3, padding=1)
-        self.conv3 = slayer.conv(32, 64, 3, padding=1)
-        self.pool1 = slayer.pool(2)
-        self.pool2 = slayer.pool(2)
-        self.fc1 = slayer.dense((8, 8, 64), 10)
+        self.SC1 = slayer.conv(2, 16, 5, padding=1)
+        self.SC2 = slayer.conv(16, 32, 3, padding=1)
+        self.SC3 = slayer.conv(32, 64, 3, padding=1)
+        self.SP1 = slayer.pool(2)
+        self.SP2 = slayer.pool(2)
+        self.SF1 = slayer.dense((8, 8, 64), 10)
 
-    def forward(self, spikeInput):
-        spikeLayer1 = self.slayer.spike(self.conv1(self.slayer.psp(spikeInput )))  # 32, 32, 16
-        spikeLayer2 = self.slayer.spike(self.pool1(self.slayer.psp(spikeLayer1)))  # 16, 16, 16
-        spikeLayer3 = self.slayer.spike(self.conv2(self.slayer.psp(spikeLayer2)))  # 16, 16, 32
-        spikeLayer4 = self.slayer.spike(self.pool2(self.slayer.psp(spikeLayer3)))  #  8,  8, 32
-        spikeLayer5 = self.slayer.spike(self.conv3(self.slayer.psp(spikeLayer4)))  #  8,  8, 64
-        spikeOut    = self.slayer.spike(self.fc1  (self.slayer.psp(spikeLayer5)))  # 10
+    def forward(self, s_in):
+        s_out1 = self.slayer.spike(self.slayer.psp(self.SC1(s_in)))    # 32, 32, 16
+        s_out2 = self.slayer.spike(self.slayer.psp(self.SP1(s_out1)))  # 16, 16, 16
+        s_out3 = self.slayer.spike(self.slayer.psp(self.SC2(s_out2)))  # 16, 16, 32
+        s_out4 = self.slayer.spike(self.slayer.psp(self.SP2(s_out3)))  #  8,  8, 32
+        s_out5 = self.slayer.spike(self.slayer.psp(self.SC3(s_out4)))  #  8,  8, 64
+        s_out  = self.slayer.spike(self.slayer.psp(self.SF1(s_out5)))  # 10
 
-        return spikeOut
+        return s_out
 
 
 if __name__ == '__main__':
