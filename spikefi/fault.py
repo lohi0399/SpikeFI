68775d2e1d468ab081643f388a6c8111cb444a7d
Theofilos Spyrou <alfatheo21@gmail.com>
2023-08-15 13:33:08 +0200

    Create RoundFault class and enhance fault module

--- spikefi/fault.py
+++ spikefi/fault.py
@@ -1,5 +1,7 @@
 from .utils.quantization import Quantizer
-from typing import List, Tuple, Callable
+from typing import List, Dict, Tuple, Callable
+from torch import Tensor
+import slayerSNN as snn
 
 # TODO: Allow for selection of the fault's effectiveness in Time ?
 # e.g., for either a neuron, or a synapse fault:
@@ -13,6 +15,12 @@ class FaultSite:
         self.dim0 = dim0
         self.set_chw(chw)
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, FaultSite):
+            return False
+
+        return self.layer == other.layer and self.dim0 == other.dim0 and self.get_chw() == other.get_chw()
+
     def set_chw(self, chw: Tuple[int]) -> None:
         if chw:
             assert len(chw) == 3
@@ -23,22 +31,26 @@ class FaultSite:
         return (self.channel, self.height, self.width)
 
     def is_random(self) -> bool:
-        return bool(self.layer)
+        return not self.layer
+
+    def unroll(self) -> Tuple:
+        return (self.dim0, self.channel, self.height, self.width, slice(None))
 
     @staticmethod
-    def pos2str(x: int, c: str = '*') -> str:
-        return c if x is None else x
+    def pos2str(x: int) -> str:
+        return '*' if x is None else ':' if x == slice(None) else str(x)
+
+    def key(self) -> Tuple:
+        dim0_key = -1 if type(self.dim0) is slice else self.dim0
+        return (self.layer, dim0_key, self.get_chw())
 
     def __repr__(self) -> str:
-        s = "Fault Site:\n"
-        s += f"  - Layer: {self.layer or '*'}\n"
-        s += f"  - Position: ({FaultSite.pos2str(self.dim0)}, {FaultSite.pos2str(self.channel)}, " + \
-            f"{FaultSite.pos2str(self.height)}, {FaultSite.pos2str(self.width)})"
-        return s
+        return f"Fault Site: layer {self.layer or '*'} " + \
+            f"at ({', '.join(list(map(FaultSite.pos2str, self.unroll()[0:4])))})"
 
 
 class FaultModel:
-    targets = {'input': 'a', 'output': 'z', 'weight': 'w', 'param': 'p'}
+    targets = {'output': 'z', 'weight': 'w', 'param': 'p'}
 
     def __init__(self, target: str, method: Callable[..., float], *args) -> None:
         assert target in FaultModel.targets.values(), "Invalid fault model target"
@@ -50,12 +62,18 @@ class FaultModel:
         self.original = None
         self.perturbed = None
 
-    def __repr__(self) -> str:
-        name = self.__class__.__name__
-        if name == FaultModel.__name__:
-            name = "Custom"
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, FaultModel):
+            return False
 
-        s = f"Fault Model: '{name}'\n"
+        return self.target == other.target and self.method == other.method and self.args == other.args
+
+    def get_name(self) -> str:
+        name = self.__class__.__name__
+        return 'Custom' if name == FaultModel.__name__ else name
+
+    def __repr__(self) -> str:
+        s = f"Fault Model: '{self.get_name()}'\n"
         s += f"  - Target: {self.target}\n"
         s += f"  - Method: {self.method.__name__}\n"
         s += "  - Arguments:"
@@ -69,6 +87,13 @@ class FaultModel:
 
         return self.perturbed
 
+    def restore(self) -> float:
+        tore = self.original
+        self.original = None
+        self.perturbed = None
+
+        return tore
+
     def is_synaptic(self) -> bool:
         return self.target == 'w'
 
@@ -79,23 +104,23 @@ class FaultModel:
         return self.target == 'p'
 
     @staticmethod
-    def set_value(original: float, value: float) -> float:
+    def set_value(original: Tensor, value: float) -> float:
         return value
 
     @staticmethod
-    def add_value(original: float, value: float) -> float:
+    def add_value(original: Tensor, value: float) -> Tensor:
         return original + value
 
     @staticmethod
-    def mul_value(original: float, value: float) -> float:
+    def mul_value(original: Tensor, value: float) -> Tensor:
         return original * value
 
     @staticmethod
-    def qua_value(original: float, qua: Quantizer) -> float:
+    def qua_value(original: Tensor, qua: Quantizer) -> Tensor:
         return qua.dequantize(qua.quantize(original))
 
     @staticmethod
-    def bfl_value(original: float, qua: Quantizer, bit: int) -> float:
+    def bfl_value(original: Tensor, qua: Quantizer, bit: int) -> Tensor:
         assert bit >= 0 and bit <= qua.precision
 
         return qua.dequantize(qua.quantize(original) ^ 2 ** bit)
@@ -137,17 +162,108 @@ class BitflippedSynapse(FaultModel):
 class Fault:
     def __init__(self, model: FaultModel, sites: List[FaultSite] = [], occurrences: int = None) -> None:
         self.model = model
-        self.occurences = max(len(sites), occurrences if occurrences else 1)
-        self.sites = sites + (self.occurences - len(sites)) * [FaultSite()]
+        self.sites = sites + [FaultSite() for _ in range(max(len(sites), occurrences or 1) - len(sites))]
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Fault):
+            return False
+
+        return self.model == other.model and \
+            sorted(self.sites, key=FaultSite.key) == sorted(other.sites, key=FaultSite.key)
 
     def is_multiple(self) -> bool:
-        return self.occurences > 1
+        return len(self) > 1
+
+    def sort(self, reverse: bool = False) -> None:
+        self.sites.sort(key=FaultSite.key, reverse=reverse)
+
+    @staticmethod
+    def sorted(fault: 'Fault', reverse: bool = False) -> 'Fault':
+        return Fault(fault.model, sorted(fault.sites, key=FaultSite.key, reverse=reverse))
+
+    def __len__(self) -> int:
+        return len(self.sites)
 
     def __repr__(self) -> str:
-        s = f"+ {self.model}\n"
-        if self.is_multiple():
-            s += f"+ Multiple Fault\n+ Occurences: {self.occurences}"
-        else:
-            s += "+ Single Fault"
+        return f"Fault: '{self.model.get_name()}' @ {len(self)} site{'s' if self.is_multiple() else ''}"
 
-        return s
+
+class FaultRound:
+    def __init__(self, lay_names: List[str]) -> None:
+        self.round = {name: [] for name in lay_names}
+        self.hooks_map = {name: 2*[False] for name in lay_names}        
+        self.stats = snn.utils.stats() # TODO: Replace with custom stats object ?
+
+    def insert(self, faults: List[Fault]) -> None:
+        for f in faults:
+            for lay_name, lay_faults in self.round.items():
+                lay_sites = [s for s in f.sites if s.layer == lay_name]
+                if not lay_sites:
+                    continue
+
+                self.hooks_map[lay_name][0] = f.model.is_synaptic() or f.model.is_parametric()
+                self.hooks_map[lay_name][1] = f.model.is_neuronal()
+
+                try:
+                    lay_fault = next(lay_fault for lay_fault in lay_faults if lay_fault.model == f.model)
+                    lay_fault.sites.extend(lay_sites)
+                except StopIteration:
+                    lay_faults.append(Fault(f.model, lay_sites))
+
+        for lay_name, lay_faults in self.round.items():
+            for lay_fault in lay_faults:
+                lay_fault.sort()
+
+    def get_faults(self, layer_name: str = None, targets: str = None) -> List[Fault]:
+        faults = self.round[layer_name] if layer_name else self.round.values()
+
+        if targets:
+            return [f for f in faults if f.model.target in targets]
+
+        return faults
+
+    def get_synapse_faults(self, layer_name: str = None) -> List[Fault]:
+        return self.get_faults(layer_name, 'w')
+
+    def get_neuron_faults(self, layer_name: str = None) -> List[Fault]:
+        return self.get_faults(layer_name, 'zp')
+
+    def get_parametric_faults(self, layer_name: str = None) -> List[Fault]:
+        return self.get_faults(layer_name, 'p')
+
+    def remove(self, faults: List[Fault]) -> None:
+        for f in faults:
+            for lay_name, lay_faults in self.round.items():
+                lay_sites = [s for s in f.sites if s.layer == lay_name]
+                if not lay_sites:
+                    continue
+
+                try:
+                    lay_fault = next(lay_fault for lay_fault in lay_faults if lay_fault.model == f.model)
+                    for lay_site in lay_sites:
+                        if lay_site in lay_fault.sites:
+                            lay_fault.sites.remove(lay_site)
+
+                    if not lay_fault.sites:
+                        lay_faults.remove(lay_fault)
+                except StopIteration:
+                    continue
+
+        self.map_hooks()
+
+    def map_hooks(self) -> Dict[str, List[bool]]:
+        self.hooks_map = {name: 2*[False] for name in list(self.round.keys())}
+
+        for lay_faults in self.round.values():
+            for f in lay_faults:
+                for s in f.sites:
+                    self.hooks_map[s.layer][0] = f.model.is_synaptic() or f.model.is_parametric()
+                    self.hooks_map[s.layer][1] = f.model.is_neuronal()
+
+        return self.hooks_map
+
+    def __contains__(self, fault: Fault) -> bool:
+        return any(fault in inner_faults for inner_faults in self.round.values())
+
+    def __repr__(self) -> str:
+        return str(self.round)
--- spikefi/utils/quantization.py
+++ spikefi/utils/quantization.py
@@ -1,6 +1,10 @@
+from torch import Tensor
+
+
 class Quantizer:
     def __init__(self, precision: int, xmin: float, xmax: float, qmin: int = 0) -> None:
         self.precision = precision
+
         self.xmin = xmin
         self.xmax = xmax
 
@@ -16,18 +20,17 @@ class Quantizer:
             self.zero_point = self.qmax
         self.zero_point = int(self.zero_point)
 
-    def quantize(self, x: float) -> int:
+    def quantize(self, x: Tensor) -> Tensor:
         assert x >= self.xmin and x <= self.xmax
 
-        q = int(self.zero_point + x / self.scale)
+        q = self.zero_point + x / self.scale
 
-        if q < self.qmin:
-            return self.qmin
-        if q > self.qmax:
-            return self.qmax
+        q.clamp_(self.qmin, self.qmax).round_()
+        q = q.round().byte()
+
         return q
 
-    def dequantize(self, q: int) -> float:
+    def dequantize(self, q: Tensor) -> Tensor:
         assert q >= self.qmin and q <= self.qmax
 
         return self.scale * (q - self.zero_point)
@@ -37,4 +40,5 @@ class Quantizer:
         s += f"  - Precision: {self.precision} bits\n"
         s += f"  - Real values range: {self.xmin} to {self.xmax}\n"
         s += f"  - Quantized values range: {self.qmin} to {self.qmax}"
+
         return s
